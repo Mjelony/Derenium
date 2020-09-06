bc2b94a896d5c42281c79bb3cc79b105730a8256
Mjelony <K@DESKTOP-GVUNU2R>
2020-09-06 21:08:13 +0200

    Stairs

---
+++ src/main/java/net/mcreator/derenium/block/ReinforcedBricksStairsBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.derenium.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.derenium.itemgroup.DereniumItemGroup;
+import net.mcreator.derenium.DereniumModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@DereniumModElements.ModElement.Tag
+public class ReinforcedBricksStairsBlock extends DereniumModElements.ModElement {
+	@ObjectHolder("derenium:reinforced_bricks_stairs")
+	public static final Block block = null;
+	public ReinforcedBricksStairsBlock(DereniumModElements instance) {
+		super(instance, 68);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(DereniumItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends StairsBlock {
+		public CustomBlock() {
+			super(new Block(Block.Properties.create(Material.ROCK)).getDefaultState(), Block.Properties.create(Material.ROCK).sound(SoundType.STONE)
+					.hardnessAndResistance(1.5f, 20f).lightValue(0).harvestLevel(3).harvestTool(ToolType.PICKAXE));
+			setRegistryName("reinforced_bricks_stairs");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
